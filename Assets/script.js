a27058b7d363cd6e9a324ad12ad31441e2605bd0
Jstaples <jstaplesmusic@gmail.com>
2022-09-28 21:38:49 -0400

    Added script file, current day of month displayed at header

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+// *GLOBAL VARIABLES*
+
+var timeDisplayEl = $('#currentDay');
+timeDisplayEl.text(moment().format("dddd MMMM Do"));
+
